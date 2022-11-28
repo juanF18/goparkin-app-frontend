4d1d157e11a3b963a9fdf0b7d604326e211ba5be
Juan Felipe <juanfelipecortescastrillon@gmail.com>
2022-11-28 13:26:11 -0500

    Se agrega la seccion de rating con la libreria react-simple-start-rating

--- src/components/Map/PopupContent/PopupContent.js
+++ src/components/Map/PopupContent/PopupContent.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaCarSide, FaMotorcycle } from "react-icons/fa";
+import { Rating } from "react-simple-star-rating";
 import "./PopupContent.css";
 
 export function PopupContent() {
+  const [valueRating, setValueRating] = useState(0);
+
+  const ratingValue = (rate) => {
+    setValueRating(rate);
+  };
+
   return (
     <div className="popup_container">
       <div className="popup_name_open">
@@ -14,17 +21,24 @@ export function PopupContent() {
       <div className="popup_price_places">
         <div className="car">
           <FaCarSide size={30} />
-          Places: <br />
-          Price :
+          <div>
+            Lugares: ? <br />
+            Precio : ?
+          </div>
         </div>
         <div className="motorcycle">
           <FaMotorcycle size={30} />
-          Places: <br />
-          Prices
+          <div>
+            Lugares: ?<br />
+            Precio: ?
+          </div>
         </div>
       </div>
       <hr />
-      <div className="popup_rating">Rating</div>
+      <div className="popup_rating">
+        <p>Calificanos</p>
+        <Rating onClick={ratingValue} initialValue={valueRating} /> <br />
+      </div>
       <hr />
       <div className="popup_comments">Comentarios</div>
       <hr />
