dd9bc51a9619b0d2cc4a4f84f4236683b6f27973
Juan Felipe Cortes <juanfelipecortescastrillon@gmail.com>
2022-11-30 23:36:38 -0500

    Se modifican los estilos de del login form, se agrega la funcionalidad de mostrar o ocultar password

--- src/components/Auth/LoginForm/LoginForm.js
+++ src/components/Auth/LoginForm/LoginForm.js
@@ -1,27 +1,33 @@
-import React from "react";
-import { Form, Button } from "react-bootstrap";
+import React, { useState } from "react";
+import { Form, Button, InputGroup } from "react-bootstrap";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 import "./LoginForm.css";
 
 export function LoginForm() {
+  const [showPassword, setShowPassword] = useState(false);
+  const onShowHidenPassword = () => setShowPassword((prevState) => !prevState);
   return (
     <Form>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
         <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <InputGroup>
+          <Form.Control
+            type={showPassword ? "text" : "password"}
+            placeholder="Password"
+          />
+          <Button onClick={onShowHidenPassword}>
+            {showPassword ? <FaEyeSlash /> : <FaEye />}
+          </Button>
+        </InputGroup>
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
+
+      <Button className="btn-login" variant="primary" type="submit">
+        Sign in
       </Button>
     </Form>
   );
--- src/layout/UnLoggedLayout/UnLoggedLayout.js
+++ src/layout/UnLoggedLayout/UnLoggedLayout.js
@@ -5,9 +5,9 @@ import "./UnLoggedLayout.css";
 export function UnLoggedLayout(props) {
   const { children } = props;
   return (
-    <>
+    <div className="unlogged-container">
       <NavBar />
       <div className="children-container">{children}</div>
-    </>
+    </div>
   );
 }
