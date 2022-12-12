ea4d68c42693458e5fc40961f284dc4b4376ed41
Juan Felipe Cortes <juanfelipecortescastrillon@gmail.com>
2022-12-11 22:51:04 -0500

    Se finaliza el formulario para agregar parquedero, faltan validaciones del formulario

--- src/components/Forms/ParkingForm/ParkingForm.js
+++ src/components/Forms/ParkingForm/ParkingForm.js
@@ -29,13 +29,97 @@ export function ParkingForm() {
       </Row>
 
       <Row className="mb-3">
-        <Form.Group as={Col}></Form.Group>
+        <Form.Group as={Col}>
+          <Form.Label>Car places</Form.Label>
+          <Form.Control name="carPlaces" type="number" />
+        </Form.Group>
+
+        <Form.Group as={Col}>
+          <Form.Label>Motorcycle places</Form.Label>
+          <Form.Control name="carPlaces" type="number" />
+        </Form.Group>
+
+        <Form.Group as={Col}>
+          <Form.Label>Opening Hour</Form.Label>
+          <Form.Control name="carPlaces" type="number" />
+        </Form.Group>
+
+        <Form.Group as={Col}>
+          <Form.Label></Form.Label>
+          <Form.Select>
+            <option value="AM">AM</option>
+            <option value="PM">PM</option>
+          </Form.Select>
+        </Form.Group>
+
+        <Form.Group as={Col}>
+          <Form.Label>Closing Hour</Form.Label>
+          <Form.Control name="carPlaces" type="number" />
+        </Form.Group>
+
+        <Form.Group as={Col}>
+          <Form.Label></Form.Label>
+          <Form.Select>
+            <option value="AM">AM</option>
+            <option value="PM">PM</option>
+          </Form.Select>
+        </Form.Group>
       </Row>
-      <Row className="mb-3">
-        <Form.Group as={Col}></Form.Group>
-      </Row>
-      <Row className="mb-3">
-        <Form.Group as={Col}></Form.Group>
+      <Row>
+        <Form.Group as={Col}>
+          <Form.Label>Open days</Form.Label>
+          <div key={"inline-checkbox"} className="mb-3">
+            <Form.Check
+              inline
+              name="Monday"
+              label="Monday"
+              value={"Monday"}
+              type="checkbox"
+            />
+            <Form.Check
+              inline
+              name="Tuesday"
+              label="Tuesday"
+              value={"Tuesday"}
+              type="checkbox"
+            />
+            <Form.Check
+              inline
+              name="Wednesday"
+              label="Wednesday"
+              value={"Wednesday"}
+              type="checkbox"
+            />
+            <Form.Check
+              inline
+              name="Thrusday"
+              label="Thrusday"
+              value={"Thrusday"}
+              type="checkbox"
+            />
+            <Form.Check
+              inline
+              name="Friday"
+              label="Friday"
+              value={"Friday"}
+              type="checkbox"
+            />
+            <Form.Check
+              inline
+              name="Saturday"
+              label="Satuday"
+              value={"Satuday"}
+              type="checkbox"
+            />
+            <Form.Check
+              inline
+              name="Sunday"
+              label="Sunday"
+              value={"Sunday"}
+              type="checkbox"
+            />
+          </div>
+        </Form.Group>
       </Row>
     </>
   );
