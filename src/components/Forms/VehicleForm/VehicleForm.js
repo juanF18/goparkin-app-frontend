01e5b746e42b22a0310bdbf2ae56751f988e1871
Juan Felipe Cortes <juanfelipecortescastrillon@gmail.com>
2022-12-15 16:49:51 -0500

    Se agrega un estado y evento para que pueda tomar el valor del typo de vehiculo

--- src/components/Forms/VehicleForm/VehicleForm.js
+++ src/components/Forms/VehicleForm/VehicleForm.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { Col, Form, Row } from "react-bootstrap";
 
 export function VehicleForm() {
+  const [value, setValue] = useState("");
   return (
     <Row className="mb-3">
       <h3>Add a vehicle</h3>
       <Form.Group as={Col}>
         <Form.Label>Type</Form.Label>
-        <Form.Select>
+        <Form.Select
+          value={value}
+          name="typeVehicle"
+          onChange={(ev) => setValue(ev.target.value)}
+        >
           <option value="motorcycle">Motorcycle</option>
           <option value="car">Car</option>
         </Form.Select>
