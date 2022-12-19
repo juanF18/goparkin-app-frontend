f46a515d189a8ca6be33253fadbcfeb6e5803a5f
sebasgiraldo1123 <sebasgiraldo1123@gmail.com>
2022-12-19 11:11:14 -0500

    Listo el registro de la persona con su rol y la conexión con el backend, falta enlazar los formularios de vehicle y parking

--- src/components/Forms/VehicleForm/VehicleForm.js
+++ src/components/Forms/VehicleForm/VehicleForm.js
@@ -1,26 +1,42 @@
 import React, { useState } from "react";
 import { Col, Form, Row } from "react-bootstrap";
+import { useFormik } from "formik";
+import { initialValuesVehicle, validationSchemaVehicle } from "./VehicleForm.data";
 
 export function VehicleForm() {
   const [value, setValue] = useState("");
+
   return (
-    <Row className="mb-3">
+    <Row>
       <h3>Add a vehicle</h3>
-      <Form.Group as={Col}>
-        <Form.Label>Type</Form.Label>
+
+      {/* Type */}
+      <Form.Group className="col-md-3" controlId="formBasicType">
+        <Form.Label>Type *</Form.Label>
         <Form.Select
           value={value}
           name="typeVehicle"
-          onChange={(ev) => setValue(ev.target.value)}
-        >
+          onChange={(ev) => setValue(ev.target.value)}>
           <option value="motorcycle">Motorcycle</option>
           <option value="car">Car</option>
         </Form.Select>
       </Form.Group>
-      <Form.Group as={Col}>
-        <Form.Label>Plate</Form.Label>
-        <Form.Control name="plate" type="text" />
+
+      {/* Plate */}
+      <Form.Group className="col-md-3" controlId="formBasicPlate">
+        <Form.Label>Plate *</Form.Label>
+        <Form.Control
+          name="plate"
+          type="plate"
+          placeholder="Enter plate"
+          onChange={formik.handleChange}
+          isInvalid={!!formik.errors.plate}
+        />
+        <Form.Control.Feedback type="invalid">
+          {formik.errors.plate}
+        </Form.Control.Feedback>
       </Form.Group>
+
     </Row>
   );
 }
